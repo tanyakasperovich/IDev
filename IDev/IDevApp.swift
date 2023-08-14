c4ce77bf58c6079d73d4c8172feb98b5a8fcde06
Tatsiana Kasperovich <tanyakasperovich@mail.ru>
2023-08-14 13:26:51 +0300

    Initial Commit

---
+++ IDev/IDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDevApp.swift
+//  IDev
+//
+//  Created by Татьяна Касперович on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct IDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
